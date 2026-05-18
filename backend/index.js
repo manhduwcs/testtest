d01e41d1c1d95eaa32508aac091585119ca0050c
manhduwcs <manhnguyenn23077@gmail.com>
2026-05-18 16:37:56 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: 'database-server',
     user: 'root',
-    password: 'root123',
+    password: 'A@123456',
     database: 'db_test'
 });
 
