82d9a2b4e370367d21730bde49075ac1a8065961
manhduwcs <manhnguyenn23077@gmail.com>
2026-05-27 11:10:27 +0700

    update backend running 0.0.0.0

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const { app } = require('./server');
 const PORT = 4321;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`[PRODUCTION] Backend running on port ${PORT}`);
 });
 // re-test CI !
